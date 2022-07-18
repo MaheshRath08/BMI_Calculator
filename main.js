e96140f53dde07004fa9c62418f49d0d59ac7271
MaheshRath08 <maheshrath.2002@gmail.com>
2022-07-18 23:33:11 +0530

    2nd

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ btnEl.addEventListener("click", ()=>{
     let bmi = w/(h**2)
     console.log(bmi)
 })
+
+function BMIclassList(e){
+    let message;
+    if(e<18.5){
+        message = "underweight"
+        return message
+    }else if(e>18.5 && e<24.9){
+        message = "Nornal"
+        return message
+    }else if(e>25 && e<29.9){
+        message = "over weight"
+        return message
+    }else if(e>=30){
+        message = "obese"
+        return message
+    }
+}
